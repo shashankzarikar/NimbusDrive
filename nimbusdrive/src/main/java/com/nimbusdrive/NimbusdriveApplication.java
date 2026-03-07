eef13405e765f801ca9bba5b73a7a7e8d67d9526
Shashank Zarikar <zarikarshashank2005@gmail.com>
2026-03-08 01:13:38 +0530

    Setup: MYSQL configuration with dedicated nimbus_user

---
+++ nimbusdrive/src/main/java/com/nimbusdrive/NimbusdriveApplication.java
@@ -0,0 +1,13 @@
+package com.nimbusdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NimbusdriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NimbusdriveApplication.class, args);
+	}
+
+}
---
+++ nimbusdrive/src/test/java/com/nimbusdrive/NimbusdriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimbusdrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NimbusdriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

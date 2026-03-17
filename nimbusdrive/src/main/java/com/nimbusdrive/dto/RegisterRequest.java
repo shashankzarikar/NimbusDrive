01107db29b69659d4d3b6a85105db61b7f24f5d0
Shashank Zarikar <zarikarshashank2005@gmail.com>
2026-03-17 12:27:27 +0530

    feat: RegisterRequest dto created

---
+++ nimbusdrive/src/main/java/com/nimbusdrive/dto/RegisterRequest.java
@@ -0,0 +1,9 @@
+package com.nimbusdrive.dto;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+     private String username;
+     private String email;
+     private String password;
+}
